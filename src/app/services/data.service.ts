b2cf6904ab13173165b29e309c126b0d914b9270
LEE13262201 <bilibidbinilibid@gmail.com>
2024-11-19 14:53:21 +0800

    API Upload

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = 'https://portfolio-inquiries.onrender.com/api';
 
   constructor(private http: HttpClient) { }
 
